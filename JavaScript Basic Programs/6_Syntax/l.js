57b13da58d475f13e6059e7ffb4fca4327bd69df
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-07 15:45:05 +0530

    Literals in JS file added

---
+++ JavaScript Basic Programs/6_Syntax/l.js
@@ -0,0 +1,5 @@
+document.getElementById("el1").innerHTML = 41;
+document.getElementById("el2").innerHTML = 31.6334;
+document.getElementById("el3").innerHTML = 10.500;
+document.getElementById("el4").innerHTML = "String in Double Quotes. Btw are trailing zeroes present in above number on webpage?";
+document.getElementById("el5").innerHTML = 'String in Single Quotes';
