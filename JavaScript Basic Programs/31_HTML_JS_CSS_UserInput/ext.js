ab33ae06058f7a91711b087c7634a166c44c1b6f
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-07-11 14:11:56 +0530

    External JS file added

---
+++ JavaScript Basic Programs/31_HTML_JS_CSS_UserInput/ext.js
@@ -0,0 +1,15 @@
+function nish()
+{
+var u = document.getElementById("username").value;
+var p = document.getElementById("password").value;
+var setu = "NoiceCurse";
+var setp = "SiB123!@#";
+if((u==setu)&&(p==setp))
+{
+location.replace("https://www.github.com/NishkarshRaj/");
+}
+else
+{
+location.replace("Wrong.html");
+}
+}
